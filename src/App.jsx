14d110ed86466a617e1a723802c9e1358db5417d
José <jg345762@gmail.com>
2022-11-23 19:24:55 -0300

    Reviews complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Review from "./components/Review";
 
 function App() {
   return (
-    <div>
-      <section>
+    <div className="min-h-screen bg-black">
+      <section className="px-5 sm:px-8 flex justify-center items-center min-h-screen">
         <div>
-          <h1>Our Reviews</h1>
+          <h1 className="text-center text-2xl sm:text-4xl font-bold text-white mb-10">
+            Our Reviews
+          </h1>
           <Review />
         </div>
       </section>
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -5,24 +5,43 @@ import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 function Review() {
   const { currentReview, decrease, increase, surprise } = useGlobalContext();
   return (
-    <div>
-      <div className="w-20">
-        <img src={currentReview.image} alt={currentReview.name} />
+    <div className="bg-white p-5 rounded text-center container sm:max-w-xl">
+      <div className="w-28 h-28 mx-auto">
+        <img
+          src={currentReview.image}
+          alt={currentReview.name}
+          className="w-full h-full rounded-full"
+        />
+      </div>
+      <div className="my-2">
+        <h2 className="capitalize text-xl sm:text-2xl font-semibold">
+          {currentReview.name}
+        </h2>
+        <h3 className="my-1 text-lg sm:text-xl capitalize text-blue-500">
+          {currentReview.job}
+        </h3>
+        <p className="text-base sm:text-lg">{currentReview.text}</p>
       </div>
       <div>
-        <h2>{currentReview.name}</h2>
-        <h3>{currentReview.job}</h3>
-        <p>{currentReview.text}</p>
-      </div>
-      <div>
-        <button onClick={() => decrease()}>
+        <button
+          onClick={() => decrease()}
+          className="text-lg sm:text-xl text-blue-500 mr-2"
+        >
           <AiOutlineArrowLeft />
         </button>
-        <button onClick={() => increase()}>
+        <button
+          onClick={() => increase()}
+          className="text-lg sm:text-xl text-blue-500"
+        >
           <AiOutlineArrowRight />
         </button>
-        <div>
-          <button onClick={() => surprise()}>Surprise Me</button>
+        <div className="mt-2">
+          <button
+            onClick={() => surprise()}
+            className="text-lg sm:text-xl text-blue-500 border border-blue-500 rounded py-1 px-4 hover:bg-blue-500 hover:text-white transition-colors"
+          >
+            Surprise Me
+          </button>
         </div>
       </div>
     </div>
